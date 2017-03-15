4e77ed54bf7faa3829b3db2d35b63b5b9a16e9ee
Huiyu Jia <hjia@dn-wlan-154-130.sns.otago.ac.nz>
2017-03-15 19:10:57 +1300

    App is finished. PlaySoundsViewController+Audio.swift is a support file from Udacity

--- PitchPerfect/PlaybackViewController.swift
+++ PitchPerfect/PlaybackViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaybackViewController: UIViewController {
-    var recordedAudioURL: URL!
+
+
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var highPitchutton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    enum ButtonTag: Int { case slow = 0,fast,low,high,echo,reverb}
+
+    @IBAction func playButtonOnClick(_ sender: UIButton) {
+        switch(ButtonTag(rawValue : sender.tag)!){
+            case .slow:
+            playSound(rate: 0.5)
+            case .fast:
+            playSound(rate: 1.5)
+            case .high:
+            playSound(pitch: 1000)
+            case .low:
+            playSound(pitch: -1000)
+            case .echo:
+            playSound(echo: true)
+            case .reverb:
+            playSound(reverb: true)
+        }
+        configureUI(.playing)
+    }
+
+    @IBAction func stopButtonOnClick(_ sender: UIButton) {
+        stopAudio();
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupAudio()
         // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
 
     override func didReceiveMemoryWarning() {
